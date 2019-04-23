d812c73cd8ddbefa70ba9bd29a10b11ea2a38b96
Jeffrey Tellew <jefftellew@gmail.com>
2019-04-22 18:16:46 -0700

    Added buttons, share party screen

--- MunchEase/app/src/main/java/edu/ucsb/munchease/MainActivity.java
+++ MunchEase/app/src/main/java/edu/ucsb/munchease/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.ucsb.munchease;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_createParty = findViewById(R.id.button_createParty);
+        button_createParty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToSharePartyActivity = new Intent(getApplicationContext(), SharePartyActivity.class);
+                startActivity(goToSharePartyActivity);
+            }
+        });
     }
 }
