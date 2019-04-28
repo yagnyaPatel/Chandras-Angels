eb59df50db9f19e3d4ba7b1c549ace6aca9915c1
Anthony Hall <anthonyhall.school@gmail.com>
2019-04-28 01:24:08 -0700

    begin setup, nothing implemented

---
+++ app/src/main/java/edu/ucsb/munchease/FirebaseAnonAuth.java
@@ -0,0 +1,13 @@
+package edu.ucsb.munchease;
+
+import com.google.firebase.auth.*;
+
+public class FirebaseAnonAuth {
+    private FirebaseAuth fbAuth;
+    private FirebaseAuth.AuthStateListener authListener;
+
+    // figure it out
+    public void function() {
+        fbAuth = FirebaseAuth.getInstance();
+    }
+}
