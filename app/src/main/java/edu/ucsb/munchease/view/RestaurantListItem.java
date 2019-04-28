6df05794ef66a96458450864d16098ea27c76293
Jeffrey Tellew <jefftellew@gmail.com>
2019-04-27 17:15:46 -0700

    Update list item

--- app/src/main/java/edu/ucsb/munchease/view/RestaurantListItem.java
+++ app/src/main/java/edu/ucsb/munchease/view/RestaurantListItem.java
@@ -1,6 +1,8 @@
 package edu.ucsb.munchease.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.support.annotation.StyleableRes;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.widget.Button;
@@ -8,8 +10,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import edu.ucsb.munchease.R;
+import edu.ucsb.munchease.data.Restaurant;
 
 public class RestaurantListItem extends ConstraintLayout {
+
+    @StyleableRes
+    int index0 = 0;
+    @StyleableRes
+    int index1 = 1;
+    @StyleableRes
+    int index2 = 2;
+    @StyleableRes
+    int index3 = 3;
+    @StyleableRes
+    int index4 = 4;
+    @StyleableRes
+    int index5 = 5;
+    @StyleableRes
+    int index6 = 6;
+
+    Restaurant restaurant;
 
     //Basic information
     TextView textView_restaurantName;
@@ -37,7 +57,35 @@ public class RestaurantListItem extends ConstraintLayout {
 
     private void init(Context context, AttributeSet attrs) {
         inflate(context, R.layout.activity_restaurant_list_item,this);
+
+        int[] sets = {R.attr.id, R.attr.link, R.attr.restaurantName, R.attr.rating, R.attr.numberOfReviews, R.attr.price, R.attr.votes};
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, sets);
+        CharSequence id = typedArray.getText(index0);
+        CharSequence link = typedArray.getText(index1);
+        CharSequence restaurantName = typedArray.getText(index2)
+        CharSequence rating = typedArray.getText(index3);
+        CharSequence numberOfReviews = typedArray.getText(index4);
+        CharSequence price = typedArray.getText(index5);
+        CharSequence votes = typedArray.getText(index6);
+        typedArray.recycle();
+
+        initComponents();
+
+        setRestaurantName(restaurantName);
+        setNumberOfReviews(numberOfReviews);
+        setPrice(price);
+        setVotes(votes);
     }
+
+    private void initComponents() {
+        textView_restaurantName = findViewById(R.id.textView_restaurantName);
+        imageView_stars = findViewById(R.id.imageView_stars);
+        textView_numberOfReviews = findViewById(R.id.textView_numberOfReviews);
+        textView_price = findViewById(R.id.textView_price);
+        textView_votes = findViewById(R.id.textView_votes);
+    }
+
+    public void setID
 
     public void setRestaurantName(CharSequence restaurantName) {
         textView_restaurantName.setText(restaurantName);
