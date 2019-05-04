fbc05369bfc0e5bed475964b8a71ad0b2efdaff2
austinquinn <austinchuq@gmail.com>
2019-05-03 22:56:18 -0700

    Checks if no internet & displays message (needs further testing)

--- MunchEase/app/src/main/java/edu/ucsb/munchease/MainActivity.java
+++ MunchEase/app/src/main/java/edu/ucsb/munchease/MainActivity.java
@@ -2,9 +2,13 @@ package edu.ucsb.munchease;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(!isInternetAvailable())
+        {
+            String noIntern = "No Internet Connection";
+            Snackbar message = Snackbar.make((CoordinatorLayout) findViewById(R.id.myCoordinatorLayout), noIntern, 8000);
+            message.show();
+        }
         Button button_createParty = findViewById(R.id.button_createParty);
         button_createParty.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,4 +32,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public boolean isInternetAvailable(){
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com");
+            //You can replace it with your name
+            return !ipAddr.equals("");
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
 }
