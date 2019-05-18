12dc912c8b317147d398d0e7b88105540bc534a6
sudarshankannan <sudarshan.kannan99@gmail.com>
2019-05-18 16:11:33 -0700

    API stuff

--- app/src/main/java/edu/ucsb/munchease/activity/Searcheable.java
+++ app/src/main/java/edu/ucsb/munchease/activity/Searcheable.java
@@ -4,20 +4,20 @@ import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.google.android.gms.common.api.Response;
-import com.yelp.fusion.client.connection.*;
-import com.yelp.fusion.client.models.*;
-
 import java.util.HashMap;
-import java.util.Map;
-
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.common.api.Response;
 import edu.ucsb.munchease.R;
-import retrofit2.Call;
-import retrofit2.Callback;
+import android.widget.TextView;
 
 public class Searcheable extends AppCompatActivity {
     String query = "";
+    final TextView textView = (TextView) findViewById(R.id.text);
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
@@ -28,20 +28,37 @@ public class Searcheable extends AppCompatActivity {
             String query = intent.getStringExtra(SearchManager.QUERY);
         }
     }
-    //String inputString = ""; //Placeholder for the input from the query
-    YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
-    public Response<SearchResponse> getResponse(){
-        YelpFusionApi yelpFusionApi = apiFactory.createAPI("_OQqAnq91MYWUPjoqbXMTIcDSpcoIcXqhbKDASfG1CQf1OXmyL7Zjf1DHPwwncAk4sOuc1YQY79xcynpQ93ewSUMfynihNmTR1ckaAWNNeNhfIVLhQ-Q04YRy_XAXHYx");
-        Map<String, String> params = new HashMap<>();
-        // general params
-        params.put("term", query);
-        Call<SearchResponse> req = yelpFusionApi.getBusinessSearch(params);
-        Response<SearchResponse> response;
-        response = call.execute();
-        return response;
-    };
-    call.enqueue(callback);
 
+    String api_key = "_OQqAnq91MYWUPjoqbXMTIcDSpcoIcXqhbKDASfG1CQf1OXmyL7Zjf1DHPwwncAk4sOuc1YQY79xcynpQ93ewSUMfynihNmTR1ckaAWNNeNhfIVLhQ-Q04YRy_XAXHYx";
+    RequestQueue queue = Volley.newRequestQueue(this);
+    String url = "https://api.yelp.com/v3/businesses/search";
+
+    public class Parameters {
+        HashMap<String, String> mParams = new HashMap<String, String>();
+
+        public Parameters() {
+            mParams.put("Authorization", "Bearer " + "_OQqAnq91MYWUPjoqbXMTIcDSpcoIcXqhbKDASfG1CQf1OXmyL7Zjf1DHPwwncAk4sOuc1YQY79xcynpQ93ewSUMfynihNmTR1ckaAWNNeNhfIVLhQ");
+            mParams.put('cache-control', "no-cache");
+        }
+    }
+
+    Parameters mParams = new Parameters();
+    // Request a string response from the provided URL.
+    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, mParams,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    // Display the first 500 characters of the response string.
+                    textView.setText("Response is: " + response.substring(0, 500));
+                }
+            }, new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            textView.setText("That didn't work!");
+        }
+    });
+    // Add the request to the RequestQueue.
+    queue.add(stringRequest);
 }
 /*
 
