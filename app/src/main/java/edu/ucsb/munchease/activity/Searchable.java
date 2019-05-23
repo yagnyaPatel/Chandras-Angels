477bd22db171c6b2732422bbcd57531c3e460369
Anthony Hall <anthonyhall.school@gmail.com>
2019-05-22 23:12:13 -0700

    AndroidManifest error

--- app/src/main/java/edu/ucsb/munchease/activity/Searchable.java
+++ app/src/main/java/edu/ucsb/munchease/activity/Searchable.java
@@ -10,11 +10,11 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.google.android.gms.common.api.Response;
+import com.android.volley.Response;
 import edu.ucsb.munchease.R;
 import android.widget.TextView;
 
-public class Searcheable extends AppCompatActivity {
+public class Searchable extends AppCompatActivity {
     String query = "";
     final TextView textView = (TextView) findViewById(R.id.text);
 
@@ -31,8 +31,9 @@ public class Searcheable extends AppCompatActivity {
 
     String api_key = "_OQqAnq91MYWUPjoqbXMTIcDSpcoIcXqhbKDASfG1CQf1OXmyL7Zjf1DHPwwncAk4sOuc1YQY79xcynpQ93ewSUMfynihNmTR1ckaAWNNeNhfIVLhQ-Q04YRy_XAXHYx";
     RequestQueue queue = Volley.newRequestQueue(this);
-    String url = "https://api.yelp.com/v3/businesses/search";
 
+    String url = "https://api.yelp.com/v3/businesses/search";
+    /**
     public class Parameters {
         HashMap<String, String> mParams = new HashMap<String, String>();
 
@@ -41,24 +42,35 @@ public class Searcheable extends AppCompatActivity {
             mParams.put("cache-control", "no-cache");
         }
     }
+     */
 
-    Parameters mParams = new Parameters();
-    // Request a string response from the provided URL.
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url, mParams,
-            new Response.Listener() {
-                @Override
-                public void onResponse(String response) {
-                    // Display the first 500 characters of the response string.
-                    textView.setText("Response is: " + response.substring(0, 500));
-                }
-            }, new Response.ErrorListener() {
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            textView.setText("That didn't work!");
-        }
-    });
-    // Add the request to the RequestQueue.
-    queue.add(stringRequest);
+    // TODO dummy
+    public void search() {
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        }) {
+            @Override
+            protected HashMap<String, String> getParams() {
+                HashMap<String, String> mParams = new HashMap<String, String>();
+                mParams.put("Authorization", "Bearer " + "_OQqAnq91MYWUPjoqbXMTIcDSpcoIcXqhbKDASfG1CQf1OXmyL7Zjf1DHPwwncAk4sOuc1YQY79xcynpQ93ewSUMfynihNmTR1ckaAWNNeNhfIVLhQ");
+                mParams.put("cache-control", "no-cache");
+                return mParams;
+            }
+        };
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 }
 /*
 
